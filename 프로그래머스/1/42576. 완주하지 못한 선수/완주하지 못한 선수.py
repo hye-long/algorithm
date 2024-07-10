91f449cd2e8250224e96cc392095edbba290e453
hye-long <159509656+hye-long@users.noreply.github.com>
2024-07-10 14:54:39 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 66.61 ms, Memory: 38.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,13 @@
+from collections import Counter
+
+def solution(participant, completion):
+    
+    participant_counter = Counter(participant)
+    completion_counter = Counter(completion)
+    
+    
+    difference = participant_counter - completion_counter
+    
+    
+    return list(difference.keys())[0]
+
