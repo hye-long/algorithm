784a761af673da05537bbdcd91a1b0145c654894
hye-long <159509656+hye-long@users.noreply.github.com>
2024-07-12 13:04:51 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,14 +1,16 @@
-from collections import defaultdict
-
 def solution(clothes):
     answer = 1
-    closet = defaultdict(list)
-
-    for item, type in clothes:
-        closet[type].append(item)
-
-    for count in closet.values():
-        answer *= (len(count) + 1)
-
+    
+    closet = {}
+    for types, val in clothes:
+        if val in closet:
+            closet[val].append(val)
+        else:
+            closet[val] = [val]
+    
+    for types in closet.values():
+        answer *= (len(types)+1)
+    
     return answer - 1
+
 
