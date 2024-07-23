8b8e7c4945369f17d4a3017ff664da58c0e8cbd5
hye-long <159509656+hye-long@users.noreply.github.com>
2024-07-23 11:51:56 +0900

    [level 0] Title: 배열 만들기 2, Time: 493.35 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -0,0 +1,14 @@
+def solution(l, r):
+    answer=[]
+    
+    for i in range(l,r+1):
+        str_i= str(i)
+        
+        if all(char in '50' for char in str_i):
+            answer.append(i)
+            
+    if not answer:
+        return[-1]
+        
+    return sorted(answer)
+    
